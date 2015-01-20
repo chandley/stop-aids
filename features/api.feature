66b71578b6452d412cfcff4403e44b2c364653f4
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-20 19:14:00 +0000

    added api.feature cucumber tests - bad setup

---
+++ features/api.feature
@@ -0,0 +1,26 @@
+Feature: 
+  In order to share data nicely
+  As an api server
+  I want to give good responses to api requests
+
+  Scenario: Show candidate data
+  Given we have a candidate
+  When I visit the candidate api address
+  Then I get JSON candidates name
+
+  Scenario: Show candidate question
+  Given we have a candidate
+  And we have some questions
+  When I visit the candidate questions api address
+  Then I get JSON question data
+
+  Scenario: Show constituency
+  Given we have a constituency
+  When I visit the constituency api
+  Then I get JSON constituency name data
+
+  Scenario: Show constituency candidates
+  Given we have a constituency
+  And we have some candidates
+  When I visit the constituency candidates api
+  Then I get JSON candidates for constituency data
---
+++ features/step_definitions/api_steps.rb
@@ -0,0 +1,52 @@
+require_relative '../../spec/rails_helper'
+
+Given(/^we have a candidate$/) do
+  Candidate.create(name: 'Alice')
+end
+
+When(/^I visit the candidate api address$/) do
+  visit '/candidates/1'
+end
+
+Then(/^I get JSON candidates name$/) do
+  expect(page).to have_content 'Alice'
+end
+
+Given(/^we have some questions$/) do
+  Question.create(ask_text: 'Would you rather red pill or blue pill?')
+  Question.create(ask_text: 'Would you rather war or famine?')
+  Question.create(ask_text: 'Would you rather beef or chicken?')
+end
+
+When(/^I visit the candidate questions api address$/) do
+  visit '/candidates/1/questions'
+end
+
+Then(/^I get JSON question data$/) do
+  expect(page).to have_content 'beef or chicken'
+end
+
+Given(/^we have a constituency$/) do
+  Constituency.create(name: 'Bethnal Green and Bow')
+end
+
+When(/^I visit the constituency api$/) do
+  visit '/constituencies/1'
+end
+
+Then(/^I get JSON constituency name data$/) do
+  expect(page).to have_content 'Bethnal Green'
+end
+
+Given(/^we have some candidates$/) do
+  Candidate.create(name: 'Alice')
+  Candidate.create(name: 'Bob')
+end
+
+When(/^I visit the constituency candidates api$/) do
+  visit '/constituencies/1/candidates'
+end
+
+Then(/^I get JSON candidates for constituency data$/) do
+  expect(page).to have_content 'Bob'
+end
