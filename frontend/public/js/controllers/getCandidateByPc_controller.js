d9d0a0b48a1f306b681bc372e016b9bb79d31703
Giorgia <giorgia1london@gmail.com>
2015-01-20 16:54:00 +0000

    accessing api

--- frontend/public/js/controllers/getCandidateByPc_controller.js
+++ frontend/public/js/controllers/getCandidateByPc_controller.js
@@ -1,26 +1,19 @@
 angular.module('QanMP').controller('getCandidatesByPC', function($scope, $http){
 $scope.getCandidates = function(){
-    $scope.listOfCandidates = []
-    var url = 'http://www.theyworkforyou.com/api/getMP?&id=&constituency=&postcode=SE1+0XP&key=CCa95kDVHTT2Dj2zmPDdYN4f&callback=JSON_CALLBACK'
+    var url = "http://www.theyworkforyou.com/api/getMP?&id=&constituency=&postcode=" + $scope.postCode + "&key=CCa95kDVHTT2Dj2zmPDdYN4f&callback=JSON_CALLBACK"
     var searchCandidate = $http.jsonp(url)
                 .success(function(candidate) {
-                 $scope.candidates = candidate
-                 // candidates.forEach(function(singleCandidate){
-                 //    $scope.listOfCandidates.push(singleCandidate)
-                    
-                 // })
-                    console.log($scope.candidates)
-                 console.log($scope.candidates.image)
+                 $scope.mpCandidate = candidate
+                    console.log($scope.mpCandidate)
                 })
-                //  .error(function(error) {
-                //   console.log('error in deleteController',error)
-                // })
-                 
+                 .error(function(error) {
+                  // console.log('error in getCandidatesByPC',error)
+                  $scope.erroR = error
+                  // $scope.myError = "Invalid Postcode"
+                  // console.log($scope.myError)
+                })                
 }
 $scope.getCandidates()
 
 
 })
-
-
-// file:///images/mps/10298.jpg
