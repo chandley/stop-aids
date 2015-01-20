3314d466197aed56d0a5bedef9dcdb9d565460e9
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-20 18:27:55 +0000

    passes candidate and question api generation

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,6 @@
 class CandidatesController < ApplicationController
   def show
-
+    @candidate = Candidate.find(params[:id])
+    render json: @candidate
   end
 end
--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -6,15 +6,20 @@ describe Candidate do
   context "when initialized" do
 
     before do
-      candidate = Candidate.new(name: 'Alice')
-      q1 = Question.new(ask_text: 'Would you rather red pill or blue pill?')
-      q2 = Question.new(ask_text: 'Would you rather war or famine?')
-      q3 = Question.new(ask_text: 'Would you rather beef or chicken?')
+      Candidate.create(name: 'Alice')
+      Question.create(ask_text: 'Would you rather red pill or blue pill?')
+      Question.create(ask_text: 'Would you rather war or famine?')
+      Question.create(ask_text: 'Would you rather beef or chicken?')
     end
 
     it 'it should show the candidates name on the apropriate page' do
       visit '/candidates/1'
       expect(page).to have_content 'Alice'
+    end
+
+    it 'it should show the questions list on the api' do
+      visit '/candidates/1/questions'
+      expect(page).to have_content 'beef or chicken'
     end
 
     # it "should not have any answered questions" do
