0372dd8d488bddfb38c78861381a1b7389ff4c85
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-21 17:49:08 +0000

    minor changes constituencies controller

--- app/controllers/constituencies_controller.rb
+++ app/controllers/constituencies_controller.rb
@@ -1,7 +1,7 @@
 class ConstituenciesController < ApplicationController
   def show
-    @contituency = Constituency.find(params[:id])
+    @constituency = {:constituency => Constituency.find(params[:id]) }
     response.headers["Content-Type"] = "application/javascript"
-    render json: {constituency :@contituency}, callback: params['callback']
+    render json: @constituency, callback: params['callback']
   end
 end
