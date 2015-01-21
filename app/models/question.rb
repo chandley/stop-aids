22a2a16afb541a541589963c7736d16f6fae41e1
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-21 21:16:35 +0000

    changed add_question to use object as parameter

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ActiveRecord::Base
   has_many :answers
 
-  def create_answer(responder_id, answer_text)
-    Answer.create(candidate_id: responder_id, response: answer_text, question_id: self.id)
+  def create_answer(responder, answer_text)
+    Answer.create(candidate_id: responder.id, response: answer_text, question_id: self.id)
   end
 
 end
