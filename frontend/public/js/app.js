7b27f77ff8be63fb6eadf3424db38888cb4924bc
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-22 16:10:25 +0000

    removed frontend folder

--- frontend/public/js/app.js
+++
@@ -1,6 +0,0 @@
-var QanMP = angular.module('QanMP',[]);
-
-  QanMP.config(function($httpProvider) {
-      $httpProvider.defaults.useXDomain = true;
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  });
