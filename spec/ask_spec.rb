bafa25fcba7d80b0dba535f888b2c01aa75d9417
John <jjkiely@gmail.com>
2015-01-22 12:18:24 +0000

    can create an ask object with candidate id, user id, and question id

--- spec/ask_spec.rb
+++ spec/ask_spec.rb
@@ -1,2 +1,20 @@
 require 'rails_helper'
 
+describe Ask do
+
+  context 'asking a question' do
+
+    before do
+      @user = User.create(email:'test@test.com', password: 'testtest', password_confirmation: 'testtest')
+      @candidate = Candidate.create(name: 'Alice')
+      @question = Question.create(ask_text: 'Would you rather red pill or blue pill?')
+    end
+
+    it 'should create an ask object with the user id, candidate id, and question id' do
+      @ask = Ask.create(user_id: @user.id, candidate_id: @candidate.id, question_id: @question.id)
+      expect(@ask.question_id).to eq @question.id
+    end
+
+  end
+
+end
