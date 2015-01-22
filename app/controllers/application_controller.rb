80913986159683c902709eecd0a3407fc0344dd4
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-22 11:16:51 +0000

    application controller relaxed protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  #protect_from_forgery unless: -> { request.headers["Content-Type"] = "application/javascript" }
+  protect_from_forgery except: [:index, :show]
 
   def render_api (json_hash)
     response.headers["Content-Type"] = "application/javascript" # prevents MIME error with js 
