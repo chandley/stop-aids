842a73f436defb330f9e2bcfd939cdb20ce0b6c5
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-23 18:31:57 +0000

    cleaned up cucumber tests not currently used

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -115,13 +115,13 @@ Then(/^we see both on an asked questions list$/) do
   expect(page).to have_content("famine")
 end
 
-When(/^the administrator adds a question by api$/) do
-  page.driver.post("/questions",
-                    :ask_text => "do you like pizza or kebabs",  
-                    )
-end
+#When(/^the administrator adds a question by api$/) do
+  # page.driver.post("/questions",
+  #                 :ask_text => "do you like pizza or kebabs",  
+  #                 )
+#end
 
-Then(/^I get JSON kebab question text$/) do
-  expect(page).to have_content("pizza or kebabs")
-end
+#Then(/^I get JSON kebab question text$/) do
+  # expect(page).to have_content("pizza or kebabs")
+#end
 
