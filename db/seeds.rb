d08f9c3e9b6fa47c9bfb7e819d65770395ddd770
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-23 13:11:35 +0000

    moved data to app/assets for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 require 'CSV'
 
 def load_data_all
-  CSV.foreach('data/candidates.csv') do |row|
+  CSV.foreach('app/assets/data/candidates.csv') do |row|
     Candidate.create(
       constituency_id: row[0], 
       name: row[5], 
@@ -18,21 +18,21 @@ def load_data_all
       )
   end
 
-  CSV.foreach('data/constituency.csv') do |row|
+  CSV.foreach('app/assets/data/constituency.csv') do |row|
     Constituency.create(
       pa_id: row[0],
       name: row[1]
       )
   end
 
-  CSV.foreach('data/parties.csv') do |row|
+  CSV.foreach('app/assets/data/parties.csv') do |row|
     Party.create(
       party_id: row[0],
       name: row[1]
       )
   end
 
-  CSV.foreach('data/questions.csv') do |row|
+  CSV.foreach('app/assets/data/questions.csv') do |row|
     Question.create(
       ask_text: row[0]
       )
