2c33c2ad2dc1f6f732df63a9297c351b843d181d
John <jjkiely@gmail.com>
2015-01-23 13:00:04 +0000

    seed file can now populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# require 'CSV'
+require 'CSV'
 
-# CSV.foreach('candidates.csv') do |row|
-#   Candidate.create(
-#     constituency_id: row[0], 
-#     name: row[5], 
-#     party_id: row[2],
-#     twitter: row[6])
-# end
+def load_data_all
+  CSV.foreach('data/candidates.csv') do |row|
+    Candidate.create(
+      constituency_id: row[0], 
+      name: row[5], 
+      party_id: row[2],
+      twitter: row[6]
+      )
+  end
+
+  CSV.foreach('data/constituency.csv') do |row|
+    Constituency.create(
+      pa_id: row[0],
+      name: row[1]
+      )
+  end
+
+  CSV.foreach('data/parties.csv') do |row|
+    Party.create(
+      party_id: row[0],
+      name: row[1]
+      )
+  end
+
+  CSV.foreach('data/questions.csv') do |row|
+    Question.create(
+      ask_text: row[0]
+      )
+  end
+
+end
+
+load_data_all
