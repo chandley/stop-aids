3d36cbdaf98ed9fe8f4500afe27242f4015b447f
John <jjkiely@gmail.com>
2015-01-27 16:11:56 +0000

    moved api key to enviroment

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -1,2 +1,6 @@
 require 'bitly'
-$bitly = Bitly.new('stopaidsgroup', 'R_6359fee3fb314b4d875398bbc0143622')
+$bitly = Bitly.new('stopaidsgroup', ENV["BITLY_API_KEY"])
+
+
+
+'R_6359fee3fb314b4d875398bbc0143622'
