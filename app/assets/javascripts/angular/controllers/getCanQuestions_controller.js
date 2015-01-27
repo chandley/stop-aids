b3791c76173c03d10723dceb8ec5168b32542f62
Giorgia <giorgia1london@gmail.com>
2015-01-27 18:40:48 +0000

    answers can be submitted

--- app/assets/javascripts/angular/controllers/getCanQuestions_controller.js
+++ app/assets/javascripts/angular/controllers/getCanQuestions_controller.js
@@ -5,10 +5,11 @@ angular.module('qAnMp').controller('getCanQuestions', function($scope, $http){
     var url = "http://localhost:3000/candidates/1/questions/unanswered"
     $http.get(url)
       .success(function(response){
+
          response.questions.forEach(function(singleQuestion,index,array){
-            // console.log(response.questions)
             question = {}
-            question.id = singleQuestion.id
+            question.id = singleQuestion.question.id
+            // console.log(question.id)
             question.wording = singleQuestion.question.ask_text
             question.total = array.length
             question.optionOne = singleQuestion.choices[0].text
@@ -17,7 +18,6 @@ angular.module('qAnMp').controller('getCanQuestions', function($scope, $http){
             question.optionTwoID = singleQuestion.choices[1].id
             $scope.candidateQuestions.push(question)
             })
-            console.log($scope.candidateQuestions[0].optionTwoID)
         })
         .error(function(error){
             console.log('request not sent')
@@ -29,11 +29,13 @@ $scope.getCanQuestions()
     $scope.submitAnswers = function(){
         // triggered with the click button
         var answersSelected = $scope.candidateQuestions[0].optionOneID
-
-        var urlToPostAnswers = "http://localhost:3000//users/1/candidates/1/asks?question_id=1&choice_id=" + answersSelected
+        var questionID = $scope.candidateQuestions[0].id
+        console.log(questionID, 'questionID')
+        var urlToPostAnswers = "http://localhost:3000//users/1/candidates/1/asks?question_id=" + questionID + "&choice_id=" + answersSelected
         $http.post(urlToPostAnswers)
             .success(function(response){
                 console.log(response, 'success')
+    console.log($scope.test, 'test')
             })
     }
 
