22b0033f8a6e64ce393cb10d37449d8207581014
Giorgia <giorgia1london@gmail.com>
2015-01-27 15:52:58 +0000

    all answers done

--- app/assets/javascripts/angular/controllers/getCanQuestions_controller.js
+++ app/assets/javascripts/angular/controllers/getCanQuestions_controller.js
@@ -1,22 +1,35 @@
-// /candidates/:candidate_id/questions/unanswered
-
 angular.module('qAnMp').controller('getCanQuestions', function($scope, $http){
 	$scope.getCanQuestions = function(){
-    $scope.display = 5
+    $scope.display = 4
+    $scope.candidateQuestions = []
     var url = "http://localhost:3000/candidates/1/questions/unanswered"
     $http.get(url)
       .success(function(response){
-    		console.log(response)
-    	})
-    	.error(function(error){
-    		console.log('request not sent')
-    	})
-
-
-  
+         response.questions.forEach(function(singleQuestion,index,array){
+            question = {}
+            question.id = singleQuestion.id
+            question.wording = singleQuestion.question.ask_text
+            question.total = array.length
+            question.optionOne = singleQuestion.choices[0].text
+            question.optionTwo = singleQuestion.choices[1].text  
+            $scope.candidateQuestions.push(question)
+            })
+        })
+        .error(function(error){
+            console.log('request not sent')
+        })
 }
 
 $scope.getCanQuestions()
+    
+    $scope.submitAnswers = function(){
+        // triggered with the click button
+        var urlToPostAnswers = "http://localhost:3000//users/1/candidates/1/asks?question_id="
+        http.post()
+    }
+
+
+
 
 
 })
