5974584075833a5bf33fc05ac47e02327f007470
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-28 17:14:44 +0000

    try returning user id json object from new user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
 
   def create
     puts post_params
-    User.create(:email => params[:email])
-    render :nothing => true
+    created_user = User.create(:email => params[:email])
+    render_api { :user_id => created_user.id }
   end
 
   def post_params
