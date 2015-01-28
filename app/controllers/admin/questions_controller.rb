0e1425e7442a5ab3b82ea44ac151af8afd4f7274
1278dale <dalestevens1278@gmail.com>
2015-01-28 13:04:18 +0000

    refactored questions controller

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -53,26 +53,26 @@ class Admin::QuestionsController < ApplicationController
 
 
   def update
-    @article = Article.find(params[:id])
+    @article = Question.find(params[:id])
 
     respond_to do |format|
       if @article.update_attributes(params[:article])
-        format.html { redirect_to admin_articles_url, notice: 'Article was successfully updated.' }
+        format.html { redirect_to admin_questions_url, notice: 'Question was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
       end
     end
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    @question = Question.find(params[:id])
+    @question.destroy
 
     respond_to do |format|
-      format.html { redirect_to admin_articles_url }
+      format.html { redirect_to admin_questions_url }
       format.json { head :no_content }
     end
   end
