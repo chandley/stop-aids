e5c86b762dd5c44153cade2261b7a1045aa49cef
John <jjkiely@gmail.com>
2015-01-28 17:43:40 +0000

    reverted reder in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     puts post_params
     created_user = User.create(:email => params[:email])
-    render_api { :user_id => created_user.id }
+    render :nothing => true
   end
 
   def post_params
